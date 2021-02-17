eb136d0273adfd6175da498a215e304796339455
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2021-02-17 14:17:15 +0300

    Добавила обработку ошибок, убрала input

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ const userElement = document.querySelector('.user-link');
 const avatar = document.querySelector('.user-avatar');
 const bio = document.querySelector('.user-bio');
 const errorMessage = document.querySelector('.error');
-const button = document.querySelector('.search-button');
-const userName = document.getElementById('name');
 
 function fetchUserData(username) {
-	fetch(`https://api.github.com/users/${username}`)
-	.then((response) => {
-  	response.json()
-    	.then((data) => {
-        userElement.textContent = data.name;
-        userElement.href = data.html_url;
-        avatar.src = data.avatar_url;
-        bio.textContent = data.bio;
-      })
-      .catch((error) => {
-        errorMessage.textContent = 'Не удалось извлечь данные';
-      });
-   })
-  .catch((error) => {
-    errorMessage.textContent = 'Информация о пользователе не доступна';
-   });
+    fetch(`https://api.github.com/users/${username}`)
+        .then((response) => {
+            if (response.status === 200) {
+                response.json()
+                    .then((data) => {
+                        userElement.textContent = data.name;
+                        userElement.href = data.html_url;
+                        avatar.src = data.avatar_url;
+                        bio.textContent = data.bio;
+                        errorMessage.textContent = '';
+                    })
+                    .catch((error) => {
+                        errorMessage.textContent = 'Не удалось извлечь данные';
+                    });
+            } else if (response.status === 404) {
+                errorMessage.textContent = 'Пользователь не найден';
+            } else {
+                errorMessage.textContent = 'Ошибка';
+            }
+        })
+        .catch((error) => {
+            errorMessage.textContent = 'Информация о пользователе не доступна';
+        });
 }
 
 const params = new URLSearchParams(window.location.search);
-fetchUserData(params.get('username'));
+const username = params.get('username') || 'IrinaRyazanskaya';
 
-button.addEventListener('click', () => {
-    const userNameValue = userName.value;
-    fetchUserData(userNameValue);
-});
+fetchUserData(username);
 
